42f6427ba2cadf586f4dbb1bf8d60a433be9c69b
kykkyn2 <kykkyn2@gmail.com>
2014-10-14 00:18:34 +0900

    file upload

--- fileupload/src/main/java/com/overfunc/fileupload/HomeController.java
+++ fileupload/src/main/java/com/overfunc/fileupload/HomeController.java
@@ -1,8 +1,12 @@
 package com.overfunc.fileupload;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +14,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 /**
  * Handles requests for the application home page.
@@ -32,6 +38,41 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/file", method = RequestMethod.GET)
+	public String fileUploadRedirect(Locale locale, Model model) {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/file/upload.htm", method = RequestMethod.POST)
+	public String fileUpload( HttpServletRequest req , HttpServletResponse res ) {
+		logger.info("FILE UPLOAD START");
+		
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) req;
+		//MultipartHttpServletRequest multipart = (MultipartHttpServletRequest) req;
+		
+		try {
+			String dirPath = "E:\\";
+			MultipartFile file = multipartRequest.getFile("img_file");
+			
+			logger.info("HTML에서 넘어온 파일 이름은 : {}",file.getName());
+			logger.info("사진 파일 이름은 : {}",file.getOriginalFilename());
+			logger.info("사진 파일 크기는 : {}",file.getSize());
+			if (file != null) {			
+				String FilePath = dirPath +"/"+file.getOriginalFilename();
+				File f = new File(FilePath);
+				file.transferTo(f);
+				res.sendRedirect("/");
+				
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
 		
 		return "home";
 	}
